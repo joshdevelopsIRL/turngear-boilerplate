af26269672a328da4373a40570cd0ce1a7fd0a4c
joshdevelopsIRL <josh@joshdevelops.com>
2024-12-07 12:54:17 -0600

    started to play with :root vars in tailwind

--- turngearmedia/tailwind.config.js
+++ turngearmedia/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "var(--primary)",
+        "secondary": "var(--secondary)",
+      }
+    },
   },
   plugins: [],
 }
