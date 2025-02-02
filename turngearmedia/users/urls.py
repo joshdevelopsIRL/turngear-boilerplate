97e70dce3fd30e4a96b68e06cdf7d8be570b13fc
joshdevelopsIRL <josh@joshdevelops.com>
2025-02-01 20:41:12 -0600

    move from htmx to alpine, will add alpine-ajax next

--- turngearmedia/users/urls.py
+++ turngearmedia/users/urls.py
@@ -6,13 +6,6 @@ from .views import hx_direct, hx_profile, hx_openprofilebar, delete
 urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-
     path('direct/', direct, name='direct'),
-    path('hx_direct/', hx_direct, name='hx_direct'),
-
     path('profile/', profile, name='profile'),
-    path('hx_profile/', hx_profile, name='hx_profile'),
-    path('hx_openprofilebar/', hx_openprofilebar, name='hx_openprofilebar'),
-
-    path('delete/', delete, name='delete'),
 ]
